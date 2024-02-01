f5ac50434e7f63db2f3f0d1e24fb7b219bf30325
Arash Rasoulian <arashrasoulian1@gmail.com>
2024-02-01 18:28:24 +0100

    crud finished

--- db/migrate/20240131170633_add_tasl_to_task.rb
+++
@@ -1,5 +0,0 @@
-class AddTaslToTask < ActiveRecord::Migration[7.1]
-  def change
-    add_column :titles, :details, :completed, default: false
-  end
-end
---
+++ db/migrate/20240201123815_add_task_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddTaskToTasks < ActiveRecord::Migration[7.1]
+  # def change
+  #   add_column :title, :details, :completed
+  # end
+end
